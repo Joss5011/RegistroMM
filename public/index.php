0fa880150d1f345485c2ec1f37525a97d15e4066
Joss5011 <joserodriguez.jrg77@gmail.com>
2024-07-06 19:43:45 -0500

    se empezo con la estructura HTML

--- public/index.php
+++ public/index.php
@@ -8,14 +8,40 @@ require_once '../app/controllers/UserController.php';
 $database = new Database($host, $db_name, $username, $password);
 $pdo = $database->connect();
 
+/*
 if ($pdo){
     echo "Conexion exitosa";
 } else {
     echo "Error al conectar";
 }
+*/
 
-$userController = new UserController($pdo);
+// $userController = new UserController($pdo);
 
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medalla Milagrosa</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <main>
+        <!-- Contenido principal de la página -->
+    </main>
+
+    <footer>
+        <!-- Pie de página -->
+    </footer>
+</body>
+
+</html>
+<?php
 // Rutas simples
 // if ($_SERVER['REQUEST_URI'] == '/users') {
 //     $users = $userController->listUsers();
